e9475176b6705a6da80a5db302a04fd206658d16
Taesik yoon <taesikyoon97@naver.com>
2022-08-13 19:49:48 +0900

    Create Schema for User

---
+++ migrations/20220813101657-create-users.js
@@ -0,0 +1,38 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      userId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        require: true,
+      },
+      nickname: {
+        type: Sequelize.STRING,
+        unique: true,
+        require: true,
+      },
+      password: {
+        type: Sequelize.STRING,
+        require: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
