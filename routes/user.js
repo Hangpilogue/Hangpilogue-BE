e8a26e5900d648b63972b48771662b3e87eb0d81
Taesik yoon <taesikyoon97@naver.com>
2022-08-13 17:16:14 +0900

    Middleware/CheckUserInfo

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,9 @@ const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/user.controller");
 const userController = new UserController();
+const signMiddleware = require("../middlewares/sign.middleware");
 
+router.use(signMiddleware);
 router.post("/dup/email", userController.checkEmail);
 router.post("/signup", userController.signup);
 
